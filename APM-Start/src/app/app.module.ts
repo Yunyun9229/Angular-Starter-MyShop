a828788f77f3d1ec0bf831702b1cf99f0df81965
yunyun9229 <chuyun.huang625@gmail.com>
2022-02-25 19:59:03 -0600

    install bootstrap and font-awesome. Create productListComponent folders and implement the html, import to the app for use.

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-
+import { ProductListComponent } from './products/productList.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    ProductListComponent
   ],
   imports: [
     BrowserModule
---
+++ APM-Start/src/app/products/productList.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'pm-products',
+  templateUrl: './productList.component.html'
+})
+
+export class ProductListComponent {
+  pageTitle: string ='Product List';
+}
