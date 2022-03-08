902ac445c8d04567a9df3aa9d675136760934110
yunyun9229 <chuyun.huang625@gmail.com>
2022-03-07 23:12:13 -0600

    add routing.

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -2,25 +2,37 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/productList.component';
 import { CovertToSpacesPipe } from './shared/covertToSpaces.pipe';
 import { StarComponent } from './shared/star.component';
+import { ProductDetailsComponent } from './products/product-details/product-details.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 
 @NgModule({
   declarations: [
     AppComponent, 
+    WelcomeComponent,
     ProductListComponent, 
     CovertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailsComponent
    
   ],
   imports: [
     BrowserModule,
     FormsModule, 
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'products', component: ProductListComponent},
+      {path: 'products/:id', component: ProductDetailsComponent},
+      {path: 'welcome', component: WelcomeComponent},
+      {path: ' ', redirectTo:'welcome', pathMatch: 'full'},
+      {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ])
   ],
   bootstrap: [AppComponent]
 })
